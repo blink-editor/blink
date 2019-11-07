96bc5bf0f5fa41f7da216cb0f098e1c7ce1e2007
Bradley Walters <bradley@walters.app>
2019-11-06 20:06:47 -0700

    Client2: Use vscode-jsonrpc to send initialize message

--- client2/src/langserver-client.ts
+++ client2/src/langserver-client.ts
@@ -1,4 +1,23 @@
 import * as net from "net"
+import * as rpc from "vscode-jsonrpc"
+
+class ConsoleLogger implements rpc.Logger, rpc.Tracer {
+	error(message: string) {
+		console.error(`[vscode-jsonrpc] ${message}`)
+	}
+
+	warn(message: string) {
+		console.warn(`[vscode-jsonrpc] ${message}`)
+	}
+
+	info(message: string) {
+		console.info(`[vscode-jsonrpc] ${message}`)
+	}
+
+	log(message: string, data?: string) {
+		console.log(`[vscode-jsonrpc] ${message} ${data}`)
+	}
+}
 
 export interface LspClient {
 	connect(): void
@@ -8,8 +27,8 @@ export interface LspClient {
 export class LspTcpClient implements LspClient {
 	private host: string
 	private port: number
-  private socket: net.Socket
-	private isConnected = false
+	private socket: net.Socket
+	private connection: rpc.MessageConnection
 
 	constructor(host: string, port: number) {
 		this.host = host
@@ -17,21 +36,44 @@ export class LspTcpClient implements LspClient {
 	}
 
 	public connect() {
-    this.socket = net.connect(this.port, this.host)
+		this.socket = net.connect(this.port, this.host)
 
-    this.socket.on("connect", () => {
-      this.isConnected = true
+		const logger = new ConsoleLogger()
 
-      this.sendInitialize()
-    })
+		this.connection = rpc.createMessageConnection(
+			new rpc.SocketMessageReader(this.socket),
+			new rpc.SocketMessageWriter(this.socket),
+			logger)
 
-    this.socket.on("data", (data) => {
-      console.log("received data", data) // TODO
-    })
+		this.connection.onClose(() => {
+			logger.log("onClose")
+		})
 
-    this.socket.on("close", (err) => {
-      this.isConnected = false
-    })
+		this.connection.onDispose(() => {
+			logger.log("onDispose")
+		})
+
+		this.connection.onError((event) => {
+			logger.log(`onError (${event})`)
+		})
+
+		this.connection.onUnhandledNotification((message) => {
+			logger.log(`onUnhandledNotification (${message})`)
+		})
+
+		this.connection.onNotification((method, params) => {
+			logger.log(`onNotification (${method}, ${params})`)
+		})
+
+		this.connection.onRequest((method, params) => {
+			logger.log(`onRequest (${method}, ${params})`)
+		})
+
+		this.connection.trace(rpc.Trace.Verbose, logger)
+
+		this.connection.listen()
+
+		this.sendInitialize()
 	}
 
 	public close() {
@@ -39,82 +81,85 @@ export class LspTcpClient implements LspClient {
 	}
 
 	private sendInitialize() {
-    if (!this.isConnected) {
-      return
-    }
+		const params = {
+			capabilities: {
+				textDocument: {
+					hover: {
+						dynamicRegistration: true,
+						contentFormat: ["plaintext", "markdown"],
+					},
+					synchronization: {
+						dynamicRegistration: true,
+						willSave: false,
+						didSave: false,
+						willSaveWaitUntil: false,
+					},
+					completion: {
+						dynamicRegistration: true,
+						completionItem: {
+							snippetSupport: false,
+							commitCharactersSupport: true,
+							documentationFormat: ["plaintext", "markdown"],
+							deprecatedSupport: false,
+							preselectSupport: false,
+						},
+						contextSupport: false,
+					},
+					signatureHelp: {
+						dynamicRegistration: true,
+						signatureInformation: {
+							documentationFormat: ["plaintext", "markdown"],
+						},
+					},
+					declaration: {
+						dynamicRegistration: true,
+						linkSupport: true,
+					},
+					definition: {
+						dynamicRegistration: true,
+						linkSupport: true,
+					},
+					typeDefinition: {
+						dynamicRegistration: true,
+						linkSupport: true,
+					},
+					implementation: {
+						dynamicRegistration: true,
+						linkSupport: true,
+					},
+				},
+				workspace: {
+					didChangeConfiguration: {
+						dynamicRegistration: true,
+					},
+				},
+				// xfilesProvider: true,
+				// xcontentProvider: true,
+			},
+			// initializationOptions: null,
+			// processId: null,
+			// rootUri: this.documentInfo.rootUri,
+			// workspaceFolders: null,
+		}
 
-    const params = {
-      capabilities: {
-        textDocument: {
-          hover: {
-            dynamicRegistration: true,
-            contentFormat: ["plaintext", "markdown"],
-          },
-          synchronization: {
-            dynamicRegistration: true,
-            willSave: false,
-            didSave: false,
-            willSaveWaitUntil: false,
-          },
-          completion: {
-            dynamicRegistration: true,
-            completionItem: {
-              snippetSupport: false,
-              commitCharactersSupport: true,
-              documentationFormat: ["plaintext", "markdown"],
-              deprecatedSupport: false,
-              preselectSupport: false,
-            },
-            contextSupport: false,
-          },
-          signatureHelp: {
-            dynamicRegistration: true,
-            signatureInformation: {
-              documentationFormat: ["plaintext", "markdown"],
-            },
-          },
-          declaration: {
-            dynamicRegistration: true,
-            linkSupport: true,
-          },
-          definition: {
-            dynamicRegistration: true,
-            linkSupport: true,
-          },
-          typeDefinition: {
-            dynamicRegistration: true,
-            linkSupport: true,
-          },
-          implementation: {
-            dynamicRegistration: true,
-            linkSupport: true,
-          },
-        },
-        workspace: {
-          didChangeConfiguration: {
-            dynamicRegistration: true,
-          },
-        },
-        // xfilesProvider: true,
-        // xcontentProvider: true,
-      },
-      // initializationOptions: null,
-      // processId: null,
-      // rootUri: this.documentInfo.rootUri,
-      // workspaceFolders: null,
-    }
+		this.connection.sendRequest("initialize", params)
+			.then((result) => {
+				console.log(`got initialize result ${result}`)
+			}, (e) => {
+				console.log(`got initialize error ${e}`)
+			})
 
-    const request = JSON.stringify({
-      jsonrpc: "2.0",
-      id: "HELLO",
-      method: "initialize",
-      params: params
-    })
+		// const request = JSON.stringify({
+		// 	jsonrpc: "2.0",
+		// 	id: "HELLO",
+		// 	method: "initialize",
+		// 	params: params
+		// })
 
-    const message = `Content-Length: ${request.length}\r\n\r\n${request}`
+		// const message = `Content-Length: ${request.length}\r\n\r\n${request}`
 
-    this.socket.write(message)
+		// this.socket.write(message)
 
-    console.log("wrote request", message)
+		// console.log("wrote request", "message")
 	}
 }
--- client2/src/renderer.ts
+++ client2/src/renderer.ts
@@ -5,6 +5,11 @@
 // selectively enable features needed in the rendering
 // process.
 
+// TODO: use better polyfill
+;(window as any).setImmediate = function(callback: (...args: any[]) => void) {
+	window.setTimeout(callback, 0)
+}
+
 setTimeout(() => {
 	console.log("connecting to server")
 	;(window as any).ConnectToServer()
