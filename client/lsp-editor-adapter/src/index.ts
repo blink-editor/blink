25c5ca1a78bdd558032d9a00992b2bf39ef3f9bf
Bradley Walters <bradley@walters.app>
2019-11-08 10:59:59 -0700

    Client2 -> Client

--- client/lsp-editor-adapter/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './ws-connection';
-export { default as LspWsConnection } from './ws-connection';
-export { default as CodeMirrorAdapter } from './codemirror-adapter';
-export * from './types';
