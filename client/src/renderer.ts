370f69005930ebfe688495103257015e3dc2854a
Ben Shapiro <bshap93@gmail.com>
2020-01-27 16:39:20 -0700

    Install Bootstrap Treeview via npm and bower

--- client/src/renderer.ts
+++ client/src/renderer.ts
@@ -543,11 +543,3 @@ function toggleProjectStructure() {
 		document.querySelector("#panes")!.classList.remove('col-8')
 	}
 }
-
-// document.getElementById("MyElement").classList.add('MyClass');
-
-// document.getElementById("MyElement").classList.remove('MyClass');
-
-// if ( document.getElementById("MyElement").classList.contains('MyClass') )
-
-// document.getElementById("MyElement").classList.toggle('MyClass');
