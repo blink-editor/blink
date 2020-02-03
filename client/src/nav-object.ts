0fc010baaf13fd312182b267a2c2c759764a4ebf
Bradley Walters <bradley@walters.app>
2020-02-03 16:34:52 -0700

    Fix bestSymbolForRange not taking into account uri

--- client/src/nav-object.ts
+++ client/src/nav-object.ts
@@ -105,15 +105,17 @@ export class NavObject {
 	 * @param loc location of desired symbol
 	 */
 	bestSymbolForLocation(loc: lsp.Location): SymbolInfo | null {
-		const findParentOfRange = (symbols: SymbolInfo[], range: lsp.Range, bestSymbol: SymbolInfo | null, bestScore: number | null): [SymbolInfo | null, number | null] => {
+		const findParentOfRange = (symbols: SymbolInfo[], location: lsp.Location, bestSymbol: SymbolInfo | null, bestScore: number | null): [SymbolInfo | null, number | null] => {
 			if (!symbols) {
 				return [bestSymbol, bestScore]
 			}
 
 			// search for tightest enclosing scope for this reference
 			for (const symbol of symbols) {
+				const range = location.range
 				// test if symbol is the tightest known bound around range
-				if (((symbol.range.start.line <= range.start.line && symbol.range.end.line >= range.end.line) // range entirely within cachedRange (inclusive)
+				if (symbol.uri === location.uri
+						&& ((symbol.range.start.line <= range.start.line && symbol.range.end.line >= range.end.line) // range entirely within cachedRange (inclusive)
 						|| ((symbol.range.start.line === range.start.line && symbol.range.start.character <= range.start.character)
 							&& (symbol.range.end.line === range.end.line && symbol.range.end.character >= range.end.character)))
 						&& (bestScore === null || symbol.range.end.line - symbol.range.start.line < bestScore) // tightest line bound so far
@@ -124,7 +126,7 @@ export class NavObject {
 				}
 				// test if children have tighter bound
 				if (symbol.children !== null && symbol.children !== undefined) {
-					const [bestSymbolOfChildren, bestScoreOfChildren] = findParentOfRange(symbol.children, range, bestSymbol, bestScore)
+					const [bestSymbolOfChildren, bestScoreOfChildren] = findParentOfRange(symbol.children, location, bestSymbol, bestScore)
 
 					if (bestScore === null || (bestScoreOfChildren !== null && bestScore !== null && bestScoreOfChildren < bestScore)) {
 						bestScore = bestScoreOfChildren
@@ -136,7 +138,7 @@ export class NavObject {
 			return [bestSymbol, bestScore]
 		}
 
-		const [symbol, score] = findParentOfRange(Array.from(this.symToInfo.values()), loc.range, null, null)
+		const [symbol, score] = findParentOfRange(Array.from(this.symToInfo.values()), loc, null, null)
 
 		return symbol
 	}
