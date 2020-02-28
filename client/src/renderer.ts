c78149e1d43ec52953a79e2a5d620e5a6724f641
Ben Roach <benroach123@gmail.com>
2020-02-27 18:53:39 -0700

    finished feature. Will integrate both versions of symbolsEqual in renderer.

--- client/src/renderer.ts
+++ client/src/renderer.ts
@@ -568,6 +568,15 @@ class Editor {
 	}
 
 	async FindCallers(targetSymbol: SymbolInfo): Promise<SymbolInfo[]> {
+		// TODO: merge with the other local function of the same name
+		const symbolsEqual = (symbolA: SymbolInfo, symbolB: SymbolInfo): boolean => {
+			return symbolA.name === symbolB.name && symbolA.uri === symbolB.uri
+			&& symbolA.range.start.line === symbolB.range.start.line
+			&& symbolA.range.start.character === symbolB.range.start.character
+			&& symbolA.range.end.line === symbolB.range.end.line
+			&& symbolA.range.end.character === symbolB.range.end.character
+		}
+
 		// TODO: make this language-agnostic
 		// determine where the cursor should be before the name of the symbol
 		const nameStartPos =
@@ -603,6 +612,18 @@ class Editor {
 			if (symbol.name === targetSymbol.name
 					&& symbol.kind === targetSymbol.kind
 					&& symbol.uri === targetSymbol.uri) {
+				continue
+			}
+
+			// if this symbol is already in callers, skip it
+			let passed = true
+			for (const sym2 of callers) {
+				if (symbolsEqual(symbol, sym2)) {
+					passed = false
+					break
+				}
+			}
+			if (!passed) {
 				continue
 			}
 
