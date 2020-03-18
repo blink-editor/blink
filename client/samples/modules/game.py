402b462f59a1de9235fc3edbaf96546c1d8d7482
ray836 <raygrant36@gmail.com>
2020-03-18 16:45:51 -0600

    changed scroll bar

--- client/samples/modules/game.py
+++ client/samples/modules/game.py
@@ -1,20 +1,31 @@
 import player
+
 import world
+
 import util
+
 import random
 
 class Test():
     def foo(self):
         class DeeplyNested():
             def bar(self):
+                print("test")
+                print("test")
+                print("test")
+                print("test")
+                print("test")
+                print("test")
                 return 42
         return DeeplyNested()
+
 
 mc = Test()
 
 def get_rand_num():
     rand = random.randint(2, 8)
     return expo_mod(2, 2222, rand)
+
 
 def expo_mod(num, expo, mod):
     """Raises <num> to the <expo> and modes by <mod>"""
@@ -29,6 +40,7 @@ def expo_mod(num, expo, mod):
         else:
             return (num * z**2) % mod
 
+
 def main():
     b = mc.foo()
     c = b.bar()
@@ -39,6 +51,5 @@ def main():
     print("Player ability:", player.get_ability(p))
     print("Monsters:\n" + "\n".join(w))
     print(f"Likelihood of failure: {util.square(get_rand_num())}%")
-
 if __name__ == "__main__":
     main()
